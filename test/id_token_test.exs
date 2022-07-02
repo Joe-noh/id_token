6da414c1fc77091663de4ed97468a3bfbb0cefcc
Joe-noh <goflb.jh@gmail.com>
2022-07-02 19:22:46 +0900

    use finch to send http request

--- test/id_token_test.exs
+++ test/id_token_test.exs
@@ -12,7 +12,7 @@ defmodule IDTokenTest do
       token = Fixtures.GoogleCerts.token()
       response = Fixtures.GoogleCerts.response()
 
-      Mock.with_mock Mojito, request: fn _, _ -> {:ok, response} end do
+      Mock.with_mock Finch, [:passthrough], request: fn _, _ -> {:ok, response} end do
         assert {:ok, payload} = IDToken.verify(token, module: IDToken.Firebase)
         assert payload |> is_map()
       end
@@ -21,7 +21,7 @@ defmodule IDTokenTest do
     test "returns error tuple on failure" do
       token = Fixtures.GoogleCerts.token()
 
-      Mock.with_mock Mojito, request: fn _, _ -> {:error, :something_went_wrong} end do
+      Mock.with_mock Finch, [:passthrough], request: fn _, _ -> {:error, :something_went_wrong} end do
         assert {:error, :something_went_wrong} == IDToken.verify(token, module: IDToken.Firebase)
       end
     end
